d34a8d6dd50d1f1d6c316a81e296f03895fa18d9
Mummer <mthannheimer@MacMummer.local>
2014-10-21 14:28:34 +0200

    mit email , aber ohne eingetragenen hoster2

---
+++ src/mummer/email_info.py
@@ -0,0 +1,7 @@
+#for mail account
+EMAIL_USE_TLS = True
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+
--- src/signups/views.py
+++ src/signups/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+#python first
+#django second
+#your apps
+#local apps
+from django.conf import settings
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
 # Create your views here.
 
 from .forms import SignUpForm
@@ -11,6 +17,13 @@ def home(request):
     if form.is_valid():
         save_it = form.save(commit=False)
         save_it.save()
+
+        #send_mail(subject, message, from_email, to_list, fail_silently=True)
+        subject = 'Thank you!'
+        message = 'Super! Voll geil Alter danke dir fett'
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [save_it.email, settings.EMAIL_HOST_USER]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
         messages.success(request, 'Thank you for Joining!')
         return HttpResponseRedirect ('/thank-you/')
     return render_to_response("signup.html",
